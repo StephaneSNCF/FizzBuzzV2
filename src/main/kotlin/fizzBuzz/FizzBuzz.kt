c60d76200ea8fb3609e10b6980133261eadfebb1
stephane_lazarevic <stephane_lazarevic@connect-tech.sncf>
2023-11-06 09:27:09 +0100

    test returns 2 et renommage fun

---
+++ src/main/kotlin/fizzBuzz/FizzBuzz.kt
@@ -0,0 +1,10 @@
+package fizzBuzz
+
+class FizzBuzz {
+
+    fun fizzBuzzFun(nb: Int): String {
+        return if (nb == 1){
+            "1"
+        } else "2"
+    }
+}
---
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -0,0 +1,18 @@
+import fizzBuzz.FizzBuzz
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class FizzBuzzTest {
+
+    @Test
+    fun given_a_number_then_it_returns_this_number_to_string(){
+        val fizzBuzz = FizzBuzz()
+        assertEquals("1", fizzBuzz.fizzBuzzFun(1))
+    }
+
+    @Test
+    fun given_a_number_then_it_returns_this_number_to_string_2(){
+        val fizzBuzz = FizzBuzz()
+        assertEquals("2", fizzBuzz.fizzBuzzFun(2))
+    }
+}
