0556f82ccf4bb3b4cb9b3d5bf119e0d6df2c8df7
stephane_lazarevic <stephane_lazarevic@connect-tech.sncf>
2023-11-06 09:45:40 +0100

    refacto variable

--- src/test/kotlin/FizzBuzzTest.kt
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -4,39 +4,35 @@ import org.junit.jupiter.api.Assertions.assertEquals
 
 class FizzBuzzTest {
 
+    private val fizzBuzz = FizzBuzz()
+
     @Test
     fun given_a_number_then_it_returns_this_number_to_string(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("1", fizzBuzz.fizzBuzzFun(1))
     }
 
     @Test
     fun given_a_number_then_it_returns_this_number_to_string_2(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("2", fizzBuzz.fizzBuzzFun(2))
     }
 
     @Test
     fun given_any_number_then_it_returns_this_number_to_string(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("94", fizzBuzz.fizzBuzzFun(94))
     }
 
     @Test
     fun given_the_number_3_then_it_returns_fizz_to_string(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("Fizz", fizzBuzz.fizzBuzzFun(3))
     }
 
     @Test
     fun given_the_number_5_then_it_returns_buzz_to_string(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("Buzz", fizzBuzz.fizzBuzzFun(5))
     }
 
     @Test
     fun given_the_number_15_then_it_returns_fizzBuzz_to_string(){
-        val fizzBuzz = FizzBuzz()
         assertEquals("FizzBuzz", fizzBuzz.fizzBuzzFun(15))
     }
 }
